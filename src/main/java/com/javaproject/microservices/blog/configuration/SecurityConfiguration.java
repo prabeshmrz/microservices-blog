3ff5fe5df058e58af6459631f7b48337d1e26473
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2022-01-13 13:34:46 +0545

    [BLG-9] PASSWORD ENCODER
    - remove password encoder bean

--- src/main/java/com/javaproject/microservices/blog/configuration/SecurityConfiguration.java
+++ src/main/java/com/javaproject/microservices/blog/configuration/SecurityConfiguration.java
@@ -15,8 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -51,11 +49,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       authenticationRequestFilter,
       UsernamePasswordAuthenticationFilter.class
     );
-  }
-
-  @Bean
-  public PasswordEncoder getPasswordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
   }
 
   @Bean
